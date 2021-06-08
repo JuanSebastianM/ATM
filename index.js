4bd98ad900cb2d3249d00b03000cafb256dd1e0f
Juan Sebastián M <cebo_052000@hotmail.com>
2021-06-07 20:15:43 -0500

    Edited .js - now it shows the images of bills after withdrawal

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
+var imageBill = []; //Image of the bills.
+imageBill["50 bill"] = "Bills images/Bill50.png";
+imageBill["20 bill"] = "Bills images/Bill20.png";
+imageBill["10 bill"] = "Bills images/Bill10.png";
+
+var text = document.getElementById("info"); //Empty <p></p> in HTML to write the result of the withdrawal.
+var imag = document.getElementById("image"); //Empty <p></p> in HTML to insert the images of the bills given.
+
+
 class Bill {
-  constructor(value, totalAmount) {
+  constructor(name, value, totalAmount) {
+    this.image = new Image();
+    this.name = name;
     this.value = value;
     this.totalAmount = totalAmount;
+    this.image.src = imageBill[this.name];
   }
 }
+
+
 var machine = []; //Bills the ATM carries and total amount of each.
-machine[0] = new Bill(200, 10);
-machine[1] = new Bill(125, 8);
-machine[2] = new Bill(50, 5);
-machine[3] = new Bill(5, 50);
+machine[0] = new Bill("50 bill", 50, 10);
+machine[1] = new Bill("20 bill", 20, 8);
+machine[2] = new Bill("10 bill", 10, 5);
 
 var billGiven = []; //Specifies which bill has been given and the amount.
 var amountBills = 0; //Amount of bills the ATM will give the client.
 var divisor = 0; //Result of the division when iterating.
 var amountMoney = 0; //Amount of money to be withdrawn.
   
-var text = document.getElementById("info");
 var submit = document.getElementById("withdraw");
 submit.addEventListener("click", giveBill);
 function giveBill() {
@@ -29,18 +41,26 @@ function giveBill() {
       } else {
         amountBills = divisor;
       }
-      billGiven.push(new Bill(bill.value, amountBills));
+      billGiven.push(new Bill(bill.name, bill.value, amountBills));
       amountMoney = amountMoney - bill.value * amountBills;
     }
   }
   if (amountMoney > 0) {
-    text.innerHTML = "The ATM lacks some amount of money for you to withdraw all that :(";
+    text.innerHTML = "The ATM lacks some amount of money for you to withdraw it all :(";
   }
   else {
       for (const given of billGiven) {
-          if (given.totalAmount > 0) { //This is so it doesn't show e.g. "0 bill(s) of 50USD.".
+        text.innerHTML = "Here you have your bills. Thank you for using our service!";
+          if (given.totalAmount > 0) {
+              for (let i = 1; i <= given.totalAmount; i++) {
+                imag.appendChild(given.image.cloneNode(true)); //To duplicate the node (the images of bills in this case)
+              }
+              console.log(given);
+          }
+        /*This is the text approach: it doesn't show e.g. "0 bill(s) of 50USD.".
+          if (given.totalAmount > 0) {
               text.innerHTML += given.totalAmount + " bill(s) of " + given.value + "USD.<br />";
-          }  
+          } */
       }
   }
 }
